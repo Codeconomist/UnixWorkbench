45e083bcea1bfdd023c451f4788a35bc45346401
codeconomist <robjchampion@hotmail.com>
2018-09-07 15:40:59 +0100

    guessinggame.sh - Added commenting.  Changed behaviour a little so it only asks the full "Enter text" phrase once (thereafter it's implied by "Try again!").

--- guessinggame.sh
+++ guessinggame.sh
@@ -13,12 +13,18 @@ function compare {
   fi
 }
 
+#  Sets the initial values of variables.
+#    file_count is set as the number of files (including folders) in the current directory at the initial script run
+#    loop_status keeps track of the continuation of the loop (and therefore also the guess status)
 file_count=$(ls | wc -l)
 loop_status=1
 
+#  Loop that continues while $loop_status is not 0.  It asks for the guess and assigns the response to $guess
+#    When a correct guess is passed to the compare function, the response causes $loop_status to set to 0
+#      The IF statement determines what text to display based on the $loop_status and the loop will exit when correct
+echo "Type how many files you think the folder contains, then press enter:"
 while [[ $loop_status -ne 0 ]]
 do
-  echo "Type how many files you think the folder contains, then press enter:"
   read guess
   let loop_status=$(compare $guess $file_count)
 
